d2bdd01f920154eb52b02aa550932b6e293ada01
Mollie Knute <mcknute@gmail.com>
2016-04-27 11:38:10 -0700

    Basic frontend for one-page application

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
+// var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
   firstName: String,
